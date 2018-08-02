05bda110f2975f688aa5e4169d4773dffef41544
zhukaihao <zhukaihaorj@163.com>
2018-08-02 14:54:21 +0800

    错误信息

--- src/utils/request.js
+++ src/utils/request.js
@@ -8,16 +8,6 @@ const service = axios.create({
   timeout: 10000 // request timeout
 })
 
-const DEFAULT_ERROR_MESSAGE = {
-  '400': ['参数错误', 'Wrong parameters'],
-  // '401': ['未登录', 'Unauthorized'],
-  // '403': ['无权限', 'Permission denied'],
-  // '404': ['资源未找到', 'Resource not found'],
-  // '409': ['信息冲突或已被占用', 'Confilic request'],
-  '429': ['请求太过频繁', 'Too many requests'],
-  '500': ['服务器内部错误', 'Internal error']
-}
-
 // request拦截器
 service.interceptors.request.use(
   config => {
@@ -49,15 +39,8 @@ service.interceptors.response.use(
         showClose: true
       })
     } else {
-      const { config, status } = response
       // TODO 401/403重定向
-      let errMsg
-      if (config.errMsg) {
-        errMsg = { ...DEFAULT_ERROR_MESSAGE, ...config.errMsg }
-      } else {
-        errMsg = { ...DEFAULT_ERROR_MESSAGE }
-      }
-      showErrMsg(errMsg, status)
+      showErrMsg(response)
     }
     return Promise.reject(error)
   }
@@ -65,12 +48,30 @@ service.interceptors.response.use(
 
 // 根据当前界面语言，显示错误信息
 // 其中一种语言缺少的时候，显示另一种
-function showErrMsg (errMsgArray, status) {
+// 优先级：config自定义 > 服务器返回的message > 默认值
+function showErrMsg (response) {
+  let errMsgArray = {
+    '400': ['参数错误', 'Wrong parameters'],
+    // '401': ['未登录', 'Unauthorized'],
+    // '403': ['无权限', 'Permission denied'],
+    // '404': ['资源未找到', 'Resource not found'],
+    // '409': ['信息冲突或已被占用', 'Confilic request'],
+    '429': ['请求太过频繁', 'Too many requests'],
+    '500': ['服务器内部错误', 'Internal error']
+  }
+
+  const { config, status } = response
+  if (response.data && response.data.message) {
+    errMsgArray[status] = [response.data.message, response.data.message]
+  }
+  if (config.errMsg) {
+    errMsgArray = { ...errMsgArray, ...config.errMsg }
+  }
+
   if (errMsgArray[status]) {
-    let errMsg =
-      i18n.locale === 'zh' ? errMsgArray[status][0] : errMsgArray[status][1]
     Message({
-      message: errMsg,
+      message:
+        i18n.locale === 'zh' ? errMsgArray[status][0] : errMsgArray[status][1],
       type: 'error',
       duration: 5000,
       showClose: true

837da997d15a5378bc02494d839a830d9247c095
zhukaihao <zhukaihaorj@163.com>
2018-08-10 14:32:42 +0800

    搜索输入框

--- src/views/layout/components/Navbar.vue
+++ src/views/layout/components/Navbar.vue
@@ -1,14 +1,12 @@
 <i18n>
 {
-  "zh": {
-    "searchPlaceholder": "搜索项目、设计师",
-    "signIn": "登录",
-    "signUp": "马上加入"
-  },
   "en": {
-    "searchPlaceholder": "Search projects or designers",
-    "signIn": "Sign in",
-    "signUp": "Sign up"
+    "搜索项目、设计师": "Search projects or designers",
+    "登录": "Sign in",
+    "马上加入": "Sign up",
+    "项目": "Project",
+    "设计师": "Designer",
+    "甲方": "Party"
   }
 }
 </i18n>
@@ -21,9 +19,21 @@
     <icon
       name="app-logo"
       @click.native="$router.push('/')"/>
-    <el-input
-      :placeholder="$t('searchPlaceholder')"
-      class="search"/>
+    <el-autocomplete
+      v-model="keyword"
+      :fetch-suggestions="onInputKeyword"
+      :placeholder="$t('搜索项目、设计师')"
+      :trigger-on-focus="false"
+      class="search"
+      popper-class="search-popover"
+      select-when-unmatched
+      @select="onSelectItem"
+    >
+      <template slot-scope="{ item }">
+        <span class="search__value">{{ item.value }}</span>
+        <el-tag type="info">{{ item.label }}↵ </el-tag>
+      </template>
+    </el-autocomplete>
     <navbar-menu
       v-if="isSignIn"
       class="links"/>
@@ -33,12 +43,12 @@
       <router-link to="/signin">
         <el-button
           class="signin-btn"
-          type="text">{{ $t('signIn') }}</el-button>
+          type="text">{{ $t('登录') }}</el-button>
       </router-link>
       <router-link to="/signup">
         <el-button
           class="signup-btn"
-          plain>{{ $t('signUp') }}</el-button>
+          plain>{{ $t('马上加入') }}</el-button>
       </router-link>
     </div>
   </el-menu>
@@ -48,9 +58,42 @@
 import NavbarMenu from './NavbarMenu'
 export default {
   components: { NavbarMenu },
+  data () {
+    return {
+      keyword: ''
+    }
+  },
   computed: {
     isSignIn () {
       return this.$store.getters.isSignIn
+    }
+  },
+  methods: {
+    onInputKeyword (keyword, cb) {
+      // eslint-disable-next-line
+      cb([{
+        value: keyword,
+        type: 'project',
+        label: this.$t('项目')
+      }, {
+        value: keyword,
+        type: 'designer',
+        label: this.$t('设计师')
+      }, {
+        value: keyword,
+        type: 'party',
+        label: this.$t('甲方')
+      }])
+    },
+    onSelectItem (item) {
+      if (!item.value) return // 有可能在没有输入时按回车
+      this.$router.push({
+        path: '/search',
+        query: {
+          type: item.type,
+          keyword: item.value
+        }
+      })
     }
   }
 }
@@ -90,6 +133,24 @@ export default {
       &:active {
         border-color: #aaa;
       }
+    }
+  }
+}
+</style>
+<style lang="scss">
+.search-popover {
+  .el-autocomplete-suggestion__wrap {
+    padding: 4px 0;
+  }
+  li {
+    padding: 4px 20px;
+    display: flex;
+    align-items: center;
+    .search__value {
+      flex: 1;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
     }
   }
 }

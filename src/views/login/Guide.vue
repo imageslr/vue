77ebead4ddd4b35bd13d3926ef0175e47c49020c
zhukaihao <zhukaihaorj@163.com>
2018-08-28 23:42:46 +0800

    首页banner

--- src/views/login/Guide.vue
+++ src/views/login/Guide.vue
@@ -13,71 +13,76 @@
 </i18n>
 
 <template>
-  <div class="main-container">
-    <el-card
-      :header="$t('平台入驻设计师')"
-      shadow="hover">
-      <el-scrollbar>
-        <ul class="list-reset designer-list">
+  <div>
+    <el-carousel
+      :autoplay="false"
+      trigger="click"
+      height="500px">
+      <el-carousel-item
+        v-for="work in works"
+        :key="work.id"
+        :work="work"
+        class="work-list-item">
+        <img
+          :src="work.photo_urls[0]"
+          class="work-list-item__img">
+        <p
+          class="work-list-item__title"
+          v-text="work.title + ' —— ' + work.user.name" />
+      </el-carousel-item>
+    </el-carousel>
+    <div class="main-container">
+      <el-card
+        :header="$t('平台入驻设计师')"
+        shadow="hover">
+        <el-scrollbar>
+          <ul class="list-reset designer-list">
+            <li
+              v-for="designer in designers"
+              :key="designer.id"
+              class="designer-list-item">
+              <router-link :to="'profile?uid=' + designer.id">
+                <my-avatar
+                  :avatar-url="designer.avatar_url"
+                  class="designer-list-item__avatar"/>
+              </router-link>
+              <router-link :to="'profile?uid=' + designer.id">
+                <h3 class="m0 mt1 mb1 f-13 ellipsis-1 black">{{ designer.name }}</h3>
+              </router-link>
+              <p class="m0 mb1 f-12 black-65 ellipsis-1">{{ designer.title || $t('设计师') }}</p>
+              <router-link :to="'profile?uid=' + designer.id">
+                <el-button
+                  size="mini"
+                >{{ $t('查看主页') }}</el-button>
+              </router-link>
+            </li>
+          </ul>
+        </el-scrollbar>
+      </el-card>
+      <el-card
+        :header="$t('业主发布的项目')"
+        shadow="hover"
+        class="mt-24">
+        <ul class="project-list">
           <li
-            v-for="designer in designers"
-            :key="designer.id"
-            class="designer-list-item">
-            <router-link :to="'profile?uid=' + designer.id">
-              <my-avatar
-                :avatar-url="designer.avatar_url"
-                class="designer-list-item__avatar"/>
-            </router-link>
-            <router-link :to="'profile?uid=' + designer.id">
-              <h3 class="m0 mt1 mb1 f-13 ellipsis-1 black">{{ designer.name }}</h3>
-            </router-link>
-            <p class="m0 mb1 f-12 black-65 ellipsis-1">{{ designer.title || $t('设计师') }}</p>
-            <router-link :to="'profile?uid=' + designer.id">
-              <el-button
-                size="mini"
-              >{{ $t('查看主页') }}</el-button>
-            </router-link>
+            v-for="project in projects"
+            :key="project.id"
+            class="project-list-item">
+            <el-tag v-if="isProcessing(project)">{{ $t('进行中') }}</el-tag>
+            <el-tag
+              v-else
+              type="info">{{ $t('已完成') }}</el-tag>
+            <router-link
+              :to="`/project/${project.id}`"
+              class="project-list-item__title"
+              v-text="project.title" />
+            <span
+              class="black-65 bold"
+              v-text="$t('设计费') + '：' + project.payment"/>
           </li>
         </ul>
-      </el-scrollbar>
-    </el-card>
-    <el-card
-      :header="$t('设计师作品')"
-      shadow="hover"
-      class="mt-24">
-      <el-scrollbar>
-        <ul class="list-reset work-list">
-          <work-item
-            v-for="work in works"
-            :key="work.id"
-            :work="work"
-            class="work-list-item" />
-        </ul>
-      </el-scrollbar>
-    </el-card>
-    <el-card
-      :header="$t('业主发布的项目')"
-      shadow="hover"
-      class="mt-24">
-      <ul class="project-list">
-        <li
-          v-for="project in projects"
-          :key="project.id"
-          class="project-list-item">
-          <el-tag v-if="isProcessing(project)">{{ $t('进行中') }}</el-tag>
-          <el-tag
-            v-else
-            type="info">{{ $t('已完成') }}</el-tag>
-          <router-link
-            :to="`/project/${project.id}`"
-            class="project-list-item__title"
-            v-text="project.title" />
-          <span
-            class="black-65 bold"
-            v-text="$t('设计费') + '：' + project.payment"/>
-        </li>
-      </ul>
-    </el-card>
+      </el-card>
+    </div>
   </div>
 </template>
 
@@ -123,19 +128,23 @@ export default {
     }
   }
 }
-.work-list {
-  white-space: nowrap;
-  margin: 0;
-  &-item {
-    display: inline-block;
-    width: 700px;
-    margin-right: 16px;
-    &:last-child {
-      margin-right: 0;
-    }
-    /deep/ p {
-      white-space: normal;
-    }
+.work-list-item {
+  &__img {
+    width: 100%;
+  }
+  &__title {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    margin: 0;
+    padding: 30px 80px;
+    font-size: 30px;
+    font-weight: 400;
+    line-height: 1.5em;
+    color: #fff;
+    vertical-align: middle;
+    text-align: center;
+    background: rgba(0, 0, 0, 0.5);
   }
 }
 .project-list {
